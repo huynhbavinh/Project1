65350b988a0f0db3d87cc59a1d62b8240c92e9e8
huynhbavinh <bavinh.congviec@gmail.com>
2021-09-15 22:39:27 +0700

    Lesson3
    migration, model

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <form action="" method="get">
+        <input type="text" name="title" value="{{ $article->title }} ">
+        <br>
+        <input type="text" name="content" value="{{ $article->content }} ">
+        <br>
+        <input type="submit" value="lưu">
+    </form>
+</div>
